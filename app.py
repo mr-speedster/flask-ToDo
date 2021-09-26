0dea594557db01bd50eaddd7fdaa0b2e6281805c
Ajith <ajithd78564@gmail.com>
2021-09-26 15:27:02 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, app,render_template,redirect,request
+ 
+app=Flask(__name__,template_folder='template')
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
